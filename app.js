b1727a420363453421c75e983781649d6c86017c
Ed Spry <ed.spry@ericsson.com>
2017-06-07 18:15:23 -0500

    Added ejs for templating and watchfiles for nodemon

--- app.js
+++ app.js
@@ -6,11 +6,18 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
 app.use(express.static('bower_components'));
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+
 app.get('/', function(req, res, next){
-    res.send('Aloha World!');
+    res.render('index', {
+        list    : ['first val', '2nd val', '3rd val'],
+        nav     : ['Services', 'Portfolio', 'About', 'Team', 'Contact']
+    });
+    // test
 });
 
 app.get('/routing', function(req, res, next){
