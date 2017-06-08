79b90126912829f0768b94d14bc8fd380b287383
torbrin <edspry1@gmail.com>
2017-06-07 22:31:54 -0500

    Section 3, Lecture 44 Adding routers

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 3000;
+var eventRouter = require('./src/routes/eventRoutes');
 
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
@@ -12,10 +13,19 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 
+
+app.use('/Events', eventRouter);
+
 app.get('/', function(req, res, next){
     res.render('index', {
-        list    : ['first val', '2nd val', '3rd val'],
-        nav     : ['Services', 'Portfolio', 'About', 'Team', 'Contact']
+        list: ['first val', '2nd val', '3rd val'],
+        nav : [ { Link: 'Services', Text: 'Services'},
+                { Link: 'Portfolio', Text: 'Portfolio'},
+                { Link: 'About', Text: 'About'},
+                { Link: 'Team', Text: 'Team'},
+                { Link: 'Contact', Text: 'Contact'},
+                { Link: 'Events', Text: 'Events'}
+        ]
     });
     // test
 });
---
+++ src/routes/eventRoutes.js
@@ -0,0 +1,92 @@
+/**
+ * Created by Ed on 6/7/2017.
+ */
+var express = require('express');
+
+var eventRouter = express.Router();
+
+var eventsData = [
+    {   name: 'Event 1',
+        description: 'the first event',
+        date: '2016.01.01',
+        time: '1:00 PM',
+        duration: '1 Hour',
+        location: {
+            streetAddr: '101 Main St.',
+            city: 'Los Angeles',
+            state: 'CA',
+            zip: '87885',
+            lon: 0,
+            lat: 0
+        },
+        capacity: 100
+    },
+    {   name: 'Event 2',
+        description: 'the second event',
+        date: '2016.02.02',
+        time: '2:00 PM',
+        duration: '2 Hour',
+        location: {
+            streetAddr: '202 Main St.',
+            city: 'Los Angeles',
+            state: 'CA',
+            zip: '87885',
+            lon: 0,
+            lat: 0
+        },
+        capacity: 200
+    },
+    {   name: 'Event 3',
+        description: 'the third event',
+        date: '2016.03.03',
+        time: '3:00 PM',
+        duration: '3 Hour',
+        location: {
+            streetAddr: '303 Main St.',
+            city: 'Los Angeles',
+            state: 'CA',
+            zip: '87885',
+            lon: 0,
+            lat: 0
+        },
+        capacity: 300
+    },
+    {   name: 'Event 4',
+        description: 'the fourth event',
+        date: '2016.04.04',
+        time: '4:00 PM',
+        duration: '4 Hour',
+        location: {
+            streetAddr: '404 Main St.',
+            city: 'Los Angeles',
+            state: 'CA',
+            zip: '87885',
+            lon: 0,
+            lat: 0
+        },
+        capacity: 400
+    }
+];
+
+eventRouter.route('/')
+    .get(function(req, res){
+        res.render('events', {
+            list: ['first event', '2nd event', '3rd event'],
+            nav: [
+                {Link: 'Services', Text: 'Services'},
+                {Link: 'Portfolio', Text: 'Portfolio'},
+                {Link: 'About', Text: 'About'},
+                {Link: 'Team', Text: 'Team'},
+                {Link: 'Contact', Text: 'Contact'},
+                {Link: 'Events', Text: 'Events'}
+            ],
+            events: eventsData
+        });
+    });
+
+eventRouter.route('/event')
+    .get(function(req, res) {
+        res.send('Hello Single Event!');
+    });
+
+module.exports = eventRouter;
